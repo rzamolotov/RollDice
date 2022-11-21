8f68db0b8a478205e2a877b561fe5d1792c8ceea
Roman Zamolotov <rzamolotov@gmail.com>
2022-11-21 23:41:13 +0300

    Initial Commit

---
+++ RollDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RollDice
+//
+//  Created by Роман Замолотов on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
